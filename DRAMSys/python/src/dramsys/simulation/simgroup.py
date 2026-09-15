3dbb895d85c7854687c1e47461fca982b8a0bab7
chihyu-box <whealandy@gmail.com>
2026-09-15 21:55:21 +0800

    Re-add DRAMSys submodule

--- DRAMSys/python/src/dramsys/simulation/simgroup.py
+++
@@ -1,9 +0,0 @@
-from dramsys.simulation.simulator import Simulator
-
-class SimGroup:
-    def __init__(self, simulators: list[Simulator]):
-        self.simulators = simulators
-
-    def run(self):
-        for simulator in self.simulators:
-            simulator.run()
